fd0be6bee2ff5ffcf25890e8653dca550b063a62
Knowledge <dirkhornung91@gmail.com>
2016-08-09 13:50:51 +0200

    Added functionality to menu buttons.

--- SidebarMenu/MapViewController.swift
+++ SidebarMenu/MapViewController.swift
@@ -15,6 +15,12 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
     }
 
     override func didReceiveMemoryWarning() {
